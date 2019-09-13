b0c8f781129668f0c3fd7fc2b620651e043d4e26
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-09-13 02:13:12 -0300

    Cambios en el proyecto

---
+++ src/api/RestApplication.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Globons
+ *
+ */
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
