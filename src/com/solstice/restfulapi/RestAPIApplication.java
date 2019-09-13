757f99f32505f7f73fd2ecea992b93f5304bde0d
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-09-13 09:01:07 -0300

    Cambios en el proyecto base

---
+++ src/com/solstice/restfulapi/RestAPIApplication.java
@@ -0,0 +1,8 @@
+package com.solstice.restfulapi;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestAPIApplication extends Application {
+}
