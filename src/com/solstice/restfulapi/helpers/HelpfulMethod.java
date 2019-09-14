5d0506565a30ceffcb05e6d1bd9ac2bdde7b6eaa
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-09-14 11:32:50 -0300

    Mejora en método IsStringNullOrEmpty

--- src/com/solstice/restfulapi/helpers/HelpfulMethod.java
+++ src/com/solstice/restfulapi/helpers/HelpfulMethod.java
@@ -2,6 +2,6 @@ package com.solstice.restfulapi.helpers;
 
 public class HelpfulMethod {
     public static boolean IsStringNullOrEmpty(String string) {
-        return string == null || string.equals("") || string.length() == 0;
+        return string == null || string.trim().equals("") || string.trim().length() == 0;
     }
 }
