91e620d225156a255c5f0dd383f59e143a832cdd
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-09-13 22:55:18 -0300

    Agrego helpers

--- src/com/solstice/restfulapi/helpers/HelpfulMethod.java
+++ src/com/solstice/restfulapi/helpers/HelpfulMethod.java
@@ -1,4 +1,7 @@
 package com.solstice.restfulapi.helpers;
 
 public class HelpfulMethod {
+    public static boolean IsStringNullOrEmpty(String string) {
+        return string == null || string.equals("") || string.length() == 0;
+    }
 }
--- src/com/solstice/restfulapi/helpers/Regex.java
+++ src/com/solstice/restfulapi/helpers/Regex.java
@@ -1,4 +1,5 @@
 package com.solstice.restfulapi.helpers;
 
 public class Regex {
+    public static final String EMAIL = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
 }
