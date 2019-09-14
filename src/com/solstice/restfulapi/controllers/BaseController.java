7f4757b50f2c30f53319919e56943453cbae6365
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-09-13 22:54:47 -0300

    Implemento para que el base controller pueda consumir y producir archivos json en charset UTF8

--- src/com/solstice/restfulapi/controllers/BaseController.java
+++ src/com/solstice/restfulapi/controllers/BaseController.java
@@ -4,7 +4,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON + "; charset=UTF8")
+@Consumes(MediaType.APPLICATION_JSON + "; charset=UTF8")
 public class BaseController {
 }
